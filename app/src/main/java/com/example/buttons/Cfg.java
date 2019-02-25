ef0e02ecd1a8caf3b9c21436fcb3ba4e68fa2f53
Вуль Владимир Станиславович <Vul_VS@steccom.llc>
2019-02-25 10:42:34 +0300

    Initial commit

---
+++ app/src/main/java/com/example/buttons/Cfg.java
@@ -0,0 +1,20 @@
+package com.example.buttons;
+
+public class Cfg
+{
+    boolean saved = false;
+
+    public Cfg() // construct
+    {
+    }
+
+    public boolean Load() // destruct
+    {
+    }
+
+    public boolean Save() // destruct
+    {
+    }
+
+
+}
